b8c521c05f8f04bdef85603f1151104ee118fa9f
Prashanth <prashanthbenny2008@github.com>
2023-05-02 00:34:54 +0530

    updated resume  download feature and styling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
+const resumeDownloadButton = document.querySelector('.downloadResume');
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
@@ -22,4 +23,15 @@ menu_item.forEach((item) => {
 		hamburger.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
 	});
+});
+
+resumeDownloadButton.addEventListener('click', () => {
+	try{
+		var link = document.createElement("a");
+		link.download = 'PrashanthBennyKoryar.pdf';
+		link.href = './resources/Resume.pdf';
+		link.target = '_blank'
+		link.click();
+	}
+	catch(ex){ console.error(ex); }
 });
